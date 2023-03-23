cadd4e81f77d8dda7b024098ad704a382fcfdc99
Sheddy <shedranifico@gmail.com>
2023-03-23 23:32:00 +0100

    portfolio edit

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -33,16 +33,15 @@ const Portfolio = () => {
                 </div>
                 <div className='blog'>
                     <h2>Contdown Timer</h2>
-                    <p>Welcome to my landing page, this is a random landing page<br/> that shows the a map of a community in ebonyi state where i <br/>
-                    did my national youth service scheme.
-                    This is one of the pages<br/> i made from playing around with Html, CSS and JavaScript</p>
+                    <p>Welcome to my giveaway page this simple page shows<br/>  the countdown time for my giveaway to close. This is a <br/>
+                     cool page which displays a written note at 
+                    the end of the<br/> giveaway. I made playing around with Html, CSS and JavaScript</p>
                     <a href="https://rainbow-conkies-91167f.netlify.app/">   <img className='img' src={timer} alt='display'/></a>
                 </div>
                 <div className='blog'>
                     <h2>To-do app</h2>
-                    <p>Welcome to my landing page, this is a random landing page<br/> that shows the a map of a community in ebonyi state where i <br/>
-                    did my national youth service scheme.
-                    This is one of the pages<br/> i made from playing around with Html, CSS and JavaScript</p>
+                    <p>Welcome to my To-do app, this is an app that help<br/> you set all your daily activities witout leaving a single thing<br/>
+                     out . you can add items, edit items and clear all items. <br/> your lists will still be on the browser even after closing</p>
                     <a href="https://grand-pony-a01842.netlify.app/">   <img className='img' src={todo} alt='display'/></a>
                 </div>
             </div>
